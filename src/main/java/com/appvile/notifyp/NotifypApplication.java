9ca7df49a024c0d41b61e5dad322730a5e0ad907
Andrew Romear <devdrew@outlook.com>
2017-12-20 10:08:57 -0500

    spring config

---
+++ src/main/java/com/appvile/notifyp/NotifypApplication.java
@@ -0,0 +1,12 @@
+package com.appvile.notifyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifypApplication.class, args);
+	}
+}
---
+++ src/test/java/com/appvile/notifyp/NotifypApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appvile.notifyp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotifypApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
